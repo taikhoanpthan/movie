e4f8624c916f68c53d8662417503906e8b38a86a
Phan Anh Mỹ <105805778+taikhoanpthan@users.noreply.github.com>
2024-08-21 21:05:38 +0700

    nav

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,15 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import "./index.scss";
+
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/'); // Navigate to home page
+  };
+
+  const handleDashboardClick = () => {
+    navigate('/dashboard'); // Navigate to dashboard page
+  };
+
   return (
     <div className="header">
-      <div className="header__left">
+      <div className="header__left" onClick={handleLogoClick}>
         <img
           src="https://th.bing.com/th/id/OIP.WOTx-YdNZKABwaJ44_8PFwHaBG?rs=1&pid=ImgDetMain"
-          alt=""
+          alt="Logo"
         />
       </div>
       <div className="header__right">
         <h1>Phim gần gũi cho mọi lứa tuổi</h1>
+        <button onClick={handleDashboardClick}>Go to Dashboard</button>
       </div>
     </div>
   );
