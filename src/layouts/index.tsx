b144b3c41ceadf67c0f745b9512193c8745c0198
Phan Anh Mỹ <105805778+taikhoanpthan@users.noreply.github.com>
2024-08-22 11:06:45 +0700

    css

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,14 +1,25 @@
-
-import Navbar from '../components/navbar'
-import { Outlet } from 'react-router-dom'
+import Navbar from "../components/navbar";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <div>
-      <Navbar/>
-      <Outlet/>
+      <Navbar />
+      <Outlet />
+      <div style={{ textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <img
+          src="https://qph.cf2.quoracdn.net/main-qimg-423be0e0ab38d327137c89e58584e3c2"
+          alt=""
+          style={{ width: "600px", maxWidth: "100%", margin: "10px 0" }}
+        />
+        <img
+          src="https://qph.cf2.quoracdn.net/main-qimg-8690f52d31a0feb0d90af7289fdc2000"
+          alt=""
+          style={{ width: "600px", maxWidth: "100%", margin: "10px 0" }}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
